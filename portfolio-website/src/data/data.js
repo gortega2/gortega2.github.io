269f6cb20d33f638568c2c8ad341f3f9194e5806
gortega2 <35354398+gortega2@users.noreply.github.com>
2022-02-14 15:13:28 -0700

    Mostly finished Projects and Skills section

---
+++ portfolio-website/src/data/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+    {
+        title: "Social Media Website",
+        description: "stuff",
+        image: "../images/go.jpeg",
+        link: "github link goes here"
+
+    },
+    {
+        title: "GO AI",
+        description: "Project from a machine learning course that has an AI learn to play GO",
+        image:'',
+        link: '',
+    },
+    {
+        title: 'BIM Rule Editor',
+        descripttion: 'Rule designer interface in Unity to help building designers create an environment in 3D',
+        image:'',
+        link:'',
+    },
+    {
+        title:'Medicam',
+        description: 'CMPUT 301 Final Project to create an Android app that allows client to document and record problems on their device and send it to caretakers',
+        image:'',
+        link:'',
+    }
+]
+
+export const skills = [
+    'JS,CSS,HTML',
+    'Python',
+    'C, C++, C#',
+    'Java',
+    'SQLite3',
+    'Django',
+    'Android Studio',
+    'React, Bootstrap, Tailwind',
+    'Git, Github',
+    'Software dev practices'
+]
+
