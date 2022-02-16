90bc5b56751769ed1802b29fbf9d7d5c2036adad
gortega2 <35354398+gortega2@users.noreply.github.com>
2022-02-16 16:54:36 -0700

    Fixed formatting and added pictures.

--- portfolio-website/src/data/data.js
+++ portfolio-website/src/data/data.js
@@ -1,28 +1,35 @@
 export const projects = [
     {
         title: "Social Media Website",
-        description: "stuff",
-        image: "../images/go.jpeg",
-        link: "github link goes here"
+        course: "CMPUT 404",
+        description: "Final project for CMPUT404, a web development course. My team was tasked to create a social media website deployed on the web where users" +
+        " can make posts, explore feeds, and see the posts from the website of other teams in the class. ",
+        image: require("../images/404_proj.png"),
+        link: "https://github.com/2021fallCMPUT404/group-cmput404-project"
 
     },
     {
         title: "GO AI",
-        description: "Project from a machine learning course that has an AI learn to play GO",
-        image:'',
-        link: '',
+        course: "CMPUT 455",
+        description: "Project for CMPUT 455, a machine learning course that has an AI learn to play GO using techniques such as search, knowledge, and simulation." 
+        + " Programmed using python.",
+        image:require("../images/go.jpeg"),
+        link: 'https://github.com/EPostill/CMPUT455Assign2',
     },
     {
         title: 'BIM Rule Editor',
-        descripttion: 'Rule designer interface in Unity to help building designers create an environment in 3D',
-        image:'',
-        link:'',
+        course: "CMPUT 401",
+        description: 'An application that was created for an actual client for CMPUT 401, a software process and product management course.'
+        + ' Rule designer interface in Unity to help building designers create an environment in 3D',
+        image:require('../images/401_proj.png'),
+        link:'https://github.com/cmput401-winter2019/bim-rule-editor#readme',
     },
     {
         title:'Medicam',
+        course: "CMPUT 301",
         description: 'CMPUT 301 Final Project to create an Android app that allows client to document and record problems on their device and send it to caretakers',
-        image:'',
-        link:'',
+        image:require('../images/301_proj.png'),
+        link:'https://github.com/CMPUT301F18T11/medicam',
     }
 ]
 
